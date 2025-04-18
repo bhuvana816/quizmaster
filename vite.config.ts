83d4cc03e06306a8d94fca38a45380566a2b82cb
bhuvana816 <bhuvanass2005@gmail.com>
2025-04-18 16:38:27 +0530

    second-commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pro/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
+},
 });
